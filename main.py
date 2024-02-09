455a8be5cfc1c65f97dc057c79001d547136b856
Artur Zajączkowski <artur.zajaczkowski76@gmail.com>
2024-02-09 10:18:35 +0100

    change txt to json and add search option

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
 
     nr_letters = random.randint(8, 10)
     nr_symbols = random.randint(2, 4)
@@ -19,7 +22,6 @@ def generate_password():
     password_letters = [random.choice(letters) for _ in range(nr_letters)]
     password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
     password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
-
 
     password_list = password_letters + password_symbols + password_numbers
     random.shuffle(password_list)
@@ -34,21 +36,50 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {website: {
+        "email": email,
+        "password": password
+    }}
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Wrong data", message="Please make sure that all fields are no empty!")
     elif "@" not in email:
         messagebox.showinfo(title="Wrong email format", message="Email should include '@' character!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}\n"
-                                                              f"Password: {password}\nIs it ok save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                email_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("data.json", "r") as data_file:
+                #reading old data
+                data = json.load(data_file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            #updating
+            data.update(new_data)
+            #write new data
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
 
+        finally:
+            website_entry.delete(0, END)
+            email_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        messagebox.showinfo(title="ERROR", message="No Data Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Your email/user name is: {email}\n Password: {password}")
+        else:
+            messagebox.showinfo(title="ERROR", message=f"There are no data for {website}")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -71,7 +102,7 @@ password_label = Label(text="Password: ")
 password_label.grid(row=3, column=0)
 
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry.grid(row=1, column=1, sticky="EW")
 website_entry.focus()
 
 email_entry = Entry(width=35)
@@ -81,10 +112,14 @@ email_entry.insert(0, "example@email.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, sticky="EW")
 
+search_button = Button(text="Search", command=find_password)
+search_button.grid(row=1, column=2, sticky="EW", pady=2, padx=5)
+
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2, sticky="EW", pady=2, padx=5)
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
+
 
 window.mainloop()
